51da6433f44b05639e3c68703709d194899c235b
Varun Malik <varun1malik@gmail.com>
2018-07-16 10:44:16 +0530

    continent data shifted to new file

--- aggregate.js
+++ aggregate.js
@@ -1,4 +1,7 @@
+// import continent from './continent';
+
 const fs = require('fs');
+const continent = require('./continent');
 
 /**
  * Aggregates GDP and Population Data by Continents..
@@ -58,27 +61,6 @@ const aggregate = async (filePath) => {
     return objArray;
   }
   const csvarray = await CSV2JSON(bufferString);
-
-
-  const continent = [['Argentina', 'South America'],
-    ['Australia', 'Oceania'],
-    ['Brazil', 'South America'],
-    ['Canada', 'North America'],
-    ['China', 'Asia'],
-    ['France', 'Europe'],
-    ['Germany', 'Europe'],
-    ['India', 'Asia'],
-    ['Indonesia', 'Asia'],
-    ['Italy', 'Europe'],
-    ['Japan', 'Asia'],
-    ['Mexico', 'North America'],
-    ['Russia', 'Asia'],
-    ['Saudi Arabia', 'Asia'],
-    ['South Africa', 'Africa'],
-    ['Republic of Korea', 'Asia'],
-    ['Turkey', 'Asia'],
-    ['United Kingdom', 'Europe'],
-    ['USA', 'North America']];
 
   const continent2 = new Map(continent);
   const continentData = {};
